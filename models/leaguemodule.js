1ada7b12adca63d574163f4e91cebc0de251e997
Sergio Núñez <sergio-n-g@hotmail.com>
2018-04-25 12:01:18 -0600

    Creating basic JSON object models
    Creating basic JSON object models for Leagues and MatchDays

---
+++ models/leaguemodule.js
@@ -0,0 +1,16 @@
+﻿/* Prototype of an League */
+exports.League = {
+    init: function (id, title, matchDayAmount, teams) {
+        this.ID = id;
+        this.Title = title;
+        this.MatchDayAmount = matchDayAmount;
+        this.Teams = teams;
+
+        return this;
+    }
+};
+
+/* Helper function to create an League object */
+exports.create = function (id, title, matchDayAmount, teams) {
+    return Object.create(exports.League).init(id, title, matchDayAmount, teams);
+};
