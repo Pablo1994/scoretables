ef314a9064cc9ac5e6e5597f3d34ea14fd1c8d25
Sergio Núñez <sergio-n-g@hotmail.com>
2018-04-25 21:19:07 -0600

    Reformatting index functionality
    Reformatting index functionality

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
-function initialize() {
+function initialize(leagueid) {
     // Setting URL and headers for request
     var options = {
-        url: 'https://scoretables.herokuapp.com/api/leagues',
-      //  proxy: 'http://proxy-atc.atlanta.hp.com:8080 ',				// Comment or remove this line when uploading code
+        url: 'https://scoretables.herokuapp.com/api/leagues/' + leagueid,
+        proxy: 'http://proxy-atc.atlanta.hp.com:8080 ',				// Comment or remove this line when uploading code
         headers: {
             'User-Agent': 'request'
         }
@@ -16,7 +16,7 @@ function initialize() {
     return new Promise(function(resolve, reject) {
     	// Do async job
         request.get(options, function(err, resp, body) {
-            if (err) {
+            if (err || !body) {
                 reject(err);
             } else {
                 resolve(JSON.parse(body));
@@ -30,20 +30,52 @@ function initialize() {
 // GET home page. 
 router.get('/', function (req, res) {
 
-	var initializePromise = initialize();
-    initializePromise.then(function(result) {
+    // Create league options.
+    res.render('index', {
+        title: "Score Table",
+        league: null
+    });
 
-		// Success
-	    res.render('index', {
-	    	title: "Score Table",
-	        league: result
-	    });
+	//var initializePromise = initialize(null);
+ //   initializePromise.then(function(result) {
 
-	    console.log("Successfully retrieved league data");
-    }, function(err) {
+ //       // Success
+ //       res.render('index', {
+ //           title: "Score Table",
+ //           league: result,
+
+ //       });
+
+	//    console.log("Successfully retrieved league data");
+ //   }, function(err) {
+ //       console.log(err);
+ //   })
+});
+
+// GET home page. 
+router.get('/:id', function (req, res) {
+
+    console.log('Received id: ' + req.params.id);
+
+    var initializePromise = initialize(req.params.id);
+    initializePromise.then(function (result) {
+
+        // Success request, show league data.
+        res.render('index', {
+            title: "Score Table",
+            league: result
+        });
+
+        console.log("Successfully retrieved league data");
+    }, function (err) {
+        // Create league options.
+        res.render('index', {
+            title: "Score Table",
+            league: null
+        });
+
         console.log(err);
     })
 });
-
 
 module.exports = router;
